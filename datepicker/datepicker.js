87d9260fecfd39a9c8d2978cf8489f359492fcea
mohamed.elkhodary <mohamed.elkhodary@itworx.com>
2015-11-30 01:00:31 +0200

    Update angular directive and list different use cases of the directive

--- datepicker/datepicker.js
+++ datepicker/datepicker.js
@@ -1,11 +1,34 @@
 angular.module('app')
-    
-.directive('commonDatepicker', function(){
-    return{
-        restrict: 'E',
-        scope: {
-          config : '='
-        },
-        templateUrl: 'datepicker/datepicker.html'
-    };
+
+.directive('kdatepicker', function() {
+    return {
+        restrict: 'A',
+        require : 'ngModel',
+        link : function (scope, element, attrs, ngModelCtrl) {
+            $(function(){
+                
+                // Read attribute values
+                var calendarType = attrs.calendartype;
+                var dateFormat = attrs.dateformat;
+                var language = attrs.language;
+                
+                $.calendarsPicker.setDefaults($.calendarsPicker.regionalOptions['']);
+                
+                // Use jquery to setup the calendar
+                var calendar = element.calendarsPicker($.extend( {calendar: $.calendars.instance(calendarType, language)},
+                                         $.calendarsPicker.regionalOptions[language]));
+                
+                /*var calendar = $.calendars.instance(calendarType, language);
+                $.calendarsPicker.regionalOptions[language];*/
+                
+                element.calendarsPicker({calendar: calendar, onSelect: function(date){
+                    scope.$apply(function () {
+                            ngModelCtrl.$setViewValue(date);
+                        });
+                }});
+                
+                element.calendarsPicker('option', {dateFormat: dateFormat});
+            });
+        }
+    }
 });
--- js/module.js
+++ js/module.js
@@ -5,9 +5,5 @@ var app = angular.module('app',[]);
 app.controller('mainController', ['$scope', function($scope){
     $scope.greeting = 'welcome!';
     $scope.test = "test";
-    
-    $scope.datepickerconfig = {
-        instance: 'ummalqura', // Use empty instance in case of geogerian calendar
-        language: 'en'
-    };
+    $scope.date = new Date();
 }]);
